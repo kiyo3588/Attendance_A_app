60298c19452b0ba0e2a68461b9fbd4773fa3581a
kiyo3588 <kiyoohara0627@email.com>
2023-07-09 02:50:11 +0900

    5. 5. 6 サクセスメッセージを表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
   end
@@ -10,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存に成功した場合は、ここに記述した処理が実行されます。
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user
     else
       render :new
     end
@@ -21,5 +23,4 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-    
 end
